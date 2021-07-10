57cbff64f5a2e94f6ad958eb69a1b56a4b1928b7
Marat Mukhutdinov <72585665+Marat5@users.noreply.github.com>
2021-07-10 13:53:10 +0800

    Исправлена навигация для неавторизованного юзера (#88)

--- src/components/organisms/NavHeader/NavHeader.tsx
+++ src/components/organisms/NavHeader/NavHeader.tsx
@@ -5,23 +5,53 @@ import { LanguageSelector } from 'components/molecules/LanguageSelector/Language
 import { Link, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { ThemeSwitch } from 'components/molecules/ThemeSwitch/ThemeSwitch';
+import { useSelector } from 'react-redux';
+import { getUserState } from 'redux/user/userSlice';
 
 export const NavHeader: FC = () => {
   const { t } = useTranslation();
   const location = useLocation();
   const { pathname } = location;
+  const { isAuth } = useSelector(getUserState);
 
   return (
     <div className={classNames('nav-header')}>
       <ThemeSwitch />
 
       <div>
-        <Link
-          to="/"
-          className={classNames('nav-header__link', { 'nav-header__link_active': pathname === '/' })}
-        >
-          {t('main')}
-        </Link>
+        {isAuth
+          ? (
+            <>
+              <Link
+                to="/"
+                className={classNames('nav-header__link', { 'nav-header__link_active': pathname === '/' })}
+              >
+                {t('main')}
+              </Link>
+              <Link
+                to="/profile"
+                className={classNames('nav-header__link', { 'nav-header__link_active': pathname === '/profile' })}
+              >
+                {t('profile')}
+              </Link>
+            </>
+          )
+          : (
+            <>
+              <Link
+                to="/login"
+                className={classNames('nav-header__link', { 'nav-header__link_active': pathname === '/login' })}
+              >
+                {t('login')}
+              </Link>
+              <Link
+                to="/registration"
+                className={classNames('nav-header__link', { 'nav-header__link_active': pathname === '/registration' })}
+              >
+                {t('registration')}
+              </Link>
+            </>
+          )}
 
         <Link
           to="/game"
@@ -42,13 +72,6 @@ export const NavHeader: FC = () => {
           className={classNames('nav-header__link', { 'nav-header__link_active': pathname === '/leaderboard' })}
         >
           {t('leaderboard')}
-        </Link>
-
-        <Link
-          to="/profile"
-          className={classNames('nav-header__link', { 'nav-header__link_active': pathname === '/profile' })}
-        >
-          {t('profile')}
         </Link>
       </div>
 
