584d9f91727a3551142dad1c5ec7fa3a13ab7fb6
Viktor Prilepin <vicpride@gmail.com>
2021-08-02 18:31:46 +0700

    Добавил фикс OAUth на проде. Добавил TODO комментарий (#115)

--- src/services/oauth/config.ts
+++ src/services/oauth/config.ts
@@ -2,7 +2,11 @@ import { IS_DEV } from '../../../webpackConfigs/env';
 
 const BASE_URL = IS_DEV
   ? 'http://localhost:5000'
-  : 'https://gamedev-ypraktikum.herokuapp.com';
+  // Временный фикс. Чтобы продакшен работал хотя бы с локалхоста
+  : 'http://localhost:5000';
+
+// TODO прописать redirect_url для продакшена, когда он будет добавлен в приложение на Yandex OAuth
+// : 'https://gamedev-ypraktikum.herokuapp.com';
 
 export const REDIRECT_URI_OAUTH = `${BASE_URL}/`;
 
